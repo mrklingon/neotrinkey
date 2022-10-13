dcb228c4334656177bbbc9985967fe5c6c75d27e
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-10-13 10:30:36 -0500

    extended forKlingon, Mando'a and English passphrases
    uses kwords.py, mwords.py, words.py to generate passphrases

--- diceware.py
+++ diceware.py
@@ -1,111 +1,80 @@
+import time
+import random
+import board
+import neopixel
 import random
 import touchio
-import board
-import time
+from ncount import * 
+
+from prt import *
+
 from words import *
+from mwords import *
+from kwords import *
 
-import neopixel
+REPL = True
 
-import usb_hid
-from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-from adafruit_hid.keycode import Keycode
 
-keyboard = Keyboard(usb_hid.devices)
-keyboard_layout = KeyboardLayoutUS(keyboard)
+def dcode():
+    codew = ""
 
+    for i in range(6):
+        codew = codew + " " + words[random.randrange(len(words))]
+    return (codew)
+
+
+def kcode():
+    codew = ""
+
+    for i in range(6):
+        codew = codew + " " + kwords[random.randrange(len(kwords))]
+    return (codew)
+
+
+def mcode():
+    codew = ""
+
+    for i in range(6):
+        codew = codew + " " + mwords[random.randrange(len(mwords))]
+    return (codew)
+
+
+code = dcode()
 
 touch1 = touchio.TouchIn(board.TOUCH1)
 touch2 = touchio.TouchIn(board.TOUCH2)
 
+done = False
 
-touched = time.monotonic()
+blinknum(1,blue)
+blinknum(2,red)
+blinknum(3,green)
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, auto_write=False)
-
-red = (20,0,0)
-green = (0,20,0)
-
-blue = (0,0,20)
-
-blank = (0,0,0)
-
-colors = [red,green,blue]
-
-def dcode():
-    codew = ""
-    for i in range(6):
-        codew = codew + " " + dwords[random.randrange(len(dwords))]
-    return (codew)
-
-pixels[0] = blank
-pixels[1] = blank
-pixels[2] = blank
-pixels[3] = blank
-pixels.show()
-
-def doColor (color):
-    pixels[0] = blank
-    pixels[1] = blank
-    pixels[2] = blank
-    pixels[3] = blank
-    pixels.show()
-    
-    for i in range(4):
-        pixels[i] = color
-        pixels.show()
-        time.sleep(.2)
-    time.sleep(.5)        
-    pixels[0] = blank
-    pixels[1] = blank
-    pixels[2] = blank
-    pixels[3] = blank
-    pixels.show()
-    
-
-def doMix ():
-    pixels[0] = red
-    pixels[1] = green
-    pixels[2] = blue
-    pixels[3] = red
-    pixels.show()
-    
-    time.sleep(.5)        
-    pixels[0] = blank
-    pixels[1] = blank
-    pixels[2] = blank
-    pixels[3] = blank
-    pixels.show()
-
-doColor(red)
-doColor(green)
-doColor(blue)
-
-code = "not yet"
-
-while True:
-    
+while not done:
     Val = 0
     
-    if time.monotonic() - touched < 0.15:
-        continue
     if touch1.value:
-        Val = 1
-        touched = time.monotonic()
-
-
+        blinknum(1,pink)
+        Val = Val + 1
+        
     if touch2.value:
-        Val = 2
-        touched = time.monotonic()
-
-
+        blinknum(2,gold)
+        Val = Val + 2
+    
     if Val == 1:
-        doColor(colors[random.randrange(3)])
-        code = dcode()
-        print (code)
-        keyboard_layout.write(code + "\n")  # Then send string.
-
+        code = mcode()
+        prt (code,REPL)
+        time.sleep(.25)
+    
     if Val == 2:
-        doMix()
-        print (code)
-        keyboard_layout.write(code + "\n")  # Then send string.
+        code = kcode()
+        prt (code,REPL)
+        time.sleep(.25)
+    if Val == 3:
+        blinknum(1,blue)
+        blinknum(2,red)
+        blinknum(3,green)
+        code = dcode()
+        prt (code,REPL)
+        time.sleep(.25)
+        
