ee054d3818d546ecc5b40094d5da94e09b2ce451
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-08-31 20:22:03 -0500

    Add files via upload
    simple Orrery simulation

---
+++ orrery.py
@@ -0,0 +1,41 @@
+import time
+import random
+import neopixel
+import board
+import touchio
+
+period = [24, 62, 100, 188]
+planets = ["Mercury", "Venus", "Earth", "Mars"]
+pix = [(62, 0, 0), (64,60,7), (0, 62, 0), (36, 0, 37)]
+loc = [0, 0 ,0,0]
+counter = [0,0,0,0]
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, auto_write=True)
+
+def clearplanets(lcx):
+    for i in range(4):
+        pixels[i] = (0,0,0)
+def showplanets(lcx):
+
+    for p in range(4):
+        pixels[loc[p]] = pix[p]
+
+def mvplanet(planet):
+    pixels[loc[planet]]=(0,0,0)
+    loc[planet] = (loc[planet]+1)%4
+    pixels[loc[planet]]=pix[planet]
+
+locate = 2 # start at Earth
+
+showplanets(loc)
+while True:
+    showplanets(loc)
+
+    for p in range(4):
+        counter[p] = counter[p]+1
+        print (str(planets[p])+":"+str(counter[p])+":"+str(loc[p]))
+        if counter[p]>period[p]:
+            mvplanet(p)
+            counter[p]=0
+
+#    time.sleep(.01)
+
