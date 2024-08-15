83964ee0257079f681858d05da8a0c9f727d1483
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2024-08-14 20:07:03 -0500

    Update ncount.py

--- ncount.py
+++ ncount.py
@@ -4,46 +4,56 @@ import board
 import neopixel
 import random
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, auto_write=False)
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, auto_write=True)
 
 
 #Define colors
 pink = (12,10,12)
 gold = (50, 40, 5)
-blue = (0,0,8) 
+blue = (0,0,8)
 orange = (25, 10, 0)
 blank = (0,0,0)
 grn = (0,20,0)
 green  = (0,20,0)
 red = (20,0,0)
-colors = [pink, gold, blue, orange, green,red,paleblue]
+zero = (5,5,5)
+colors = [pink, gold, blue, orange,green,red]
+
 
 def docolor(color):
     for i in range(4):
         pixels[i] = color
-    
-    
+
+
     pixels.show()
     time.sleep(.25)
 
     for i in range(4):
         pixels[i] = blank
-   
-    pixels.show()    
+
+    pixels.show()
 
 
 def blinknum(num,color):
     for i in range(num):
         docolor(color)
         time.sleep(.25)
-        
+
+def compthink(): #blink out all the colors when computer "thinking"
+    for clr in colors:
+        blinknum(1,clr)
+
 def binnum(num,color):
     docolor(blank)
     num = num % 16
-    for i in range(4):
-        if ((2**i)&num)>0:
-            pixels[i] = color
-            
+    if num == 0:
+        pixels.fill(zero)
+    else:
+        for i in range(4):
+            if ((2**i)&num)>0:
+                pixels[i] = color
+
     pixels.show()
-    
+    time.sleep(.5)
+
 
